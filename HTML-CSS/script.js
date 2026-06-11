5e6d32ebbebc58c5cf73c0820677b628c1101155
3200584-alt <3200584@dsengg.ac.in>
2026-06-11 14:09:39 +0530

    js

---
+++ HTML-CSS/script.js
@@ -0,0 +1,76 @@
+/*let name="kumar";
+var age=26;
+const height = "1.72";
+console.log(age);
+console.log(age+5);
+console.log(age-3);
+console.log(age*2);
+console.log(age/2);
+console.log(age%4);
+age+=3;
+console.log(age);
+age-=3;
+let age1="26";
+console.log(age>age1);
+console.log(age<age1);
+console.log(age>=age1);
+console.log(age<=age1);
+console.log(age===age1);
+let food1 = "Briyani";
+let break_fast = "Idly";
+let food2 = "Idly";
+let food3 = "Pongal";
+let food4 = "Poori";
+let food5 = "Dosa";
+if (food1==break_fast) {
+    console.log("It's nice to Have Briyani as in the Morning");
+}
+else if(food2==break_fast)
+{
+    console.log("It's nice to Have Idly as in the Morning");
+}
+for(let i=1;i<=30;i++)
+{
+    console.log("Day "+i+" completed");
+    }
+let i=1;
+while(;i<=30;)
+{
+    console.log("Day "+i+" completed");
+    i++;
+}
+let user_name="Dinesh";
+let password="Dinesh@10";
+let name="anil";
+let pin="anil@2006";
+let name_1="kishoor";
+let pin_1="anil@1977";
+
+if(name=="anil"){
+    console.log(user_name);
+    if(pin=="anil@1977")
+    {
+        console.log("loged in sucessfull");
+    }
+}
+for(let i=1;i<=10;i++)
+{
+    for(let j=1;j<=10;j++)
+    {
+    console.log(i+" X "+j+" = "+(j*i));
+    }
+}
+function cook()
+{
+    console.log("Pour the Flour in Tawa");
+    console.log("Rub it round order with gareta");
+    console.log("spill some oil above it then wait");
+    console.log("Twist it with dosa gareta");
+    console.log("Atlast gently serve it to Dinesh");
+}
+for (let i=1;i<=5;i++){
+cook();}*/
+function dishwash(vessels){
+    console.log("We have to wash "+vessels+" Vessels");    
+}
+dishwash(2);
