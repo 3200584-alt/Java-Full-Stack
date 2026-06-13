d315c63ab36d22d8997cf105665c4494db615fee
3200584-alt <3200584@dsengg.ac.in>
2026-06-13 09:09:24 +0530

    Events

---
+++ HTML-CSS/Manipulation.js
@@ -0,0 +1,6 @@
+
+let value1 = document.getElementById("wish");
+value1.innerHTML="<h4><u>Happy Birthday LeelaSree</u></h4>";
+//value1.innerText="Happy Birthday";
+value1.style.fontSize="50px";
+value1.style.fontStyle="italic";
